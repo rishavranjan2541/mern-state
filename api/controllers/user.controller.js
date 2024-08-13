8859376acd33bb9edb19dc5d3923584bd9ed713c
rishavranjan2541 <rishavranjan2541@gmail.com>
2024-08-13 23:52:06 +0530

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) =>{
     res.json({
@@ -32,7 +33,7 @@ export const updateUser = async(req, res, next) =>{
 };
 
 export const deleteUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id)
+    if (req.user.id === req.params.id)
       return next(errorHandler(401, 'You can only delete your own account!'));
     try {
       await User.findByIdAndDelete(req.params.id);
@@ -42,3 +43,14 @@ export const deleteUser = async (req, res, next) => {
       next(error);
     }
   };
+
+export const getUserListings = async(req, res, next) =>{
+    if(req.user.id === req.params.id){
+        try {
+            const listings = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
